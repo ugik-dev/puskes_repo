ab2a4195dcfc434d94451ba0488bee0c6506ffd6
ugik <ugik.dev@gmail.com>
2022-04-15 19:21:57 +0700

    ss

--- application/views/template/header_menu.php
+++ application/views/template/header_menu.php
@@ -78,73 +78,9 @@
 										<nav class="collapse">
 											<ul class="nav nav-pills" id="mainNav">
 												<li class="dropdown dropdown-full-color dropdown-light">
-													<a class="dropdown-item dropdown-toggle" href="<?= base_url() ?>">
+													<a class="dropdown-item dropdown-toggle active" href="<?= base_url() ?>">
 														Home
 													</a>
-													<ul class="dropdown-menu">
-														<li>
-															<a class="dropdown-item" href="<?= base_url() ?>">
-																Landing Page
-															</a>
-														</li>
-														<li>
-															<a class="dropdown-item" href="<?= base_url() ?>#demos">
-																Demos <span class="tip tip-dark">hot</span>
-															</a>
-														</li>
-														<li class="dropdown-submenu">
-															<a class="dropdown-item" href="#">Classic</a>
-															<ul class="dropdown-menu">
-																<li><a class="dropdown-item" href="index-classic.html">Classic - Original</a></li>
-																<li><a class="dropdown-item" href="index-classic-color.html">Classic - Color</a></li>
-																<li><a class="dropdown-item" href="index-classic-light.html">Classic - Light</a></li>
-																<li><a class="dropdown-item" href="index-classic-video.html">Classic - Video</a></li>
-																<li><a class="dropdown-item" href="index-classic-video-light.html">Classic - Video - Light</a></li>
-															</ul>
-														</li>
-														<li class="dropdown-submenu">
-															<a class="dropdown-item" href="#">Corporate</a>
-															<ul class="dropdown-menu">
-																<li><a class="dropdown-item" href="index-corporate.html">Corporate - Version 1</a></li>
-																<li><a class="dropdown-item" href="index-corporate-2.html">Corporate - Version 2</a></li>
-																<li><a class="dropdown-item" href="index-corporate-3.html">Corporate - Version 3</a></li>
-																<li><a class="dropdown-item" href="index-corporate-4.html">Corporate - Version 4</a></li>
-																<li><a class="dropdown-item" href="index-corporate-5.html">Corporate - Version 5</a></li>
-																<li><a class="dropdown-item" href="index-corporate-6.html">Corporate - Version 6</a></li>
-																<li><a class="dropdown-item" href="index-corporate-7.html">Corporate - Version 7</a></li>
-																<li><a class="dropdown-item" href="index-corporate-8.html">Corporate - Version 8</a></li>
-																<li><a class="dropdown-item" href="index-corporate-9.html">Corporate - Version 9</a></li>
-																<li><a class="dropdown-item" href="index-corporate-10.html" ->Corporate - Version 10</a></li>
-																<li><a class="dropdown-item" href="<?= base_url() ?>#demos" ->More...</a></li>
-															</ul>
-														</li>
-														<li class="dropdown-submenu">
-															<a class="dropdown-item" href="#">Portfolio</a>
-															<ul class="dropdown-menu">
-																<li><a class="dropdown-item" href="index-portfolio.html">Portfolio - Version 1</a></li>
-																<li><a class="dropdown-item" href="index-portfolio-2.html">Portfolio - Version 2</a></li>
-																<li><a class="dropdown-item" href="index-portfolio-3.html">Portfolio - Version 3</a></li>
-																<li><a class="dropdown-item" href="index-portfolio-4.html">Portfolio - Version 4</a></li>
-																<li><a class="dropdown-item" href="index-portfolio-5.html">Portfolio - Version 5</a></li>
-															</ul>
-														</li>
-														<li class="dropdown-submenu">
-															<a class="dropdown-item" href="#">Blog</a>
-															<ul class="dropdown-menu">
-																<li><a class="dropdown-item" href="index-blog.html">Blog - Version 1</a></li>
-																<li><a class="dropdown-item" href="index-blog-2.html">Blog - Version 2</a></li>
-																<li><a class="dropdown-item" href="index-blog-3.html">Blog - Version 3</a></li>
-																<li><a class="dropdown-item" href="index-blog-4.html">Blog - Version 4</a></li>
-																<li><a class="dropdown-item" href="index-blog-5.html">Blog - Version 5</a></li>
-															</ul>
-														</li>
-														<li class="dropdown-submenu">
-															<a class="dropdown-item" href="#">One Page</a>
-															<ul class="dropdown-menu">
-																<li><a class="dropdown-item" href="index-one-page.html">One Page Original</a></li>
-															</ul>
-														</li>
-													</ul>
 												</li>
 												<?php
 												$menu = getMenu();
@@ -164,7 +100,7 @@
 												}
 												?>
 												<li class="dropdown dropdown-full-color dropdown-light">
-													<a class="dropdown-item dropdown-toggle active" href="#" id="menu_profil">
+													<a class="dropdown-item dropdown-toggle " href="#" id="menu_profil">
 														Pegawai
 													</a>
 												</li>
