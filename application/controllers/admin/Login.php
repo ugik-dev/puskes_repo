bb47e69305d7e436fe94997eb48f8a3cd8dbc6eb
ugik <ugik.dev@gmail.com>
2022-09-17 12:24:56 +0700

    vd

--- application/controllers/admin/Login.php
+++ application/controllers/admin/Login.php
@@ -63,13 +63,15 @@ class Login extends CI_Controller
             $this->session->set_userdata('user', $u);
             $xcadmin = $cadmin->row_array();
             if ($xcadmin['pengguna_level'] == '1') {
-                // echo '2';
+                echo '2';
+                var_dump($this->session->userdata());
                 $this->session->set_userdata('akses', '1');
+                var_dump($this->session->userdata());
                 $idadmin = $xcadmin['pengguna_id'];
                 $user_nama = $xcadmin['pengguna_nama'];
                 $this->session->set_userdata('idadmin', $idadmin);
                 $this->session->set_userdata('nama', $user_nama);
-                redirect('admin/dashboard');
+                // redirect('admin/dashboard');
             } else {
                 // echo '3';
                 $this->session->set_userdata('akses', '2');
