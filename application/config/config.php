511bcad72e0b209f36d159aa4185ffb86363c951
ugik <ugik.dev@gmail.com>
2022-09-18 13:31:49 +0700

    123

--- application/config/config.php
+++ application/config/config.php
@@ -384,20 +384,20 @@ $config['encryption_key'] = 'TRUE';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 0;
-$config['sess_save_path'] = sys_get_temp_dir();
-$config['sess_match_ip'] = FALSE;
-$config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = FALSE;
-
-// $config['sess_driver'] = 'database';
-// $config['sess_cookie_name'] = 'ci_sessions';
-// $config['sess_expiration'] = 7200;
-// $config['sess_save_path'] = 'ci_sessions'; //its your table name name
+// $config['sess_driver'] = 'files';
+// $config['sess_cookie_name'] = 'ci_session';
+// $config['sess_expiration'] = 0;
+// $config['sess_save_path'] = sys_get_temp_dir();
 // $config['sess_match_ip'] = FALSE;
 // $config['sess_time_to_update'] = 300;
+// $config['sess_regenerate_destroy'] = FALSE;
+
+$config['sess_driver'] = 'database';
+$config['sess_cookie_name'] = 'ci_sessions';
+$config['sess_expiration'] = 7200;
+$config['sess_save_path'] = 'ci_sessions'; //its your table name name
+$config['sess_match_ip'] = FALSE;
+$config['sess_time_to_update'] = 300;
 
 
 /*
