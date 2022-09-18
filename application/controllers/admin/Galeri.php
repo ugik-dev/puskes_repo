e8c4fc820595c170953920bca03d4ae830b34476
ugik <ugik.dev@gmail.com>
2022-09-18 13:49:08 +0700

    12332

--- application/controllers/admin/Galeri.php
+++ application/controllers/admin/Galeri.php
@@ -34,16 +34,16 @@ class Galeri extends CI_Controller
 			if ($this->upload->do_upload('filefoto')) {
 				$gbr = $this->upload->data();
 				//Compress Image
-				$config['image_library'] = 'gd2';
-				$config['source_image'] = './upload/images/' . $gbr['file_name'];
-				$config['create_thumb'] = FALSE;
-				$config['maintain_ratio'] = FALSE;
-				$config['quality'] = '60%';
-				$config['width'] = 500;
-				$config['height'] = 400;
-				$config['new_image'] = './upload/images/' . $gbr['file_name'];
-				$this->load->library('image_lib', $config);
-				$this->image_lib->resize();
+				// $config['image_library'] = 'gd2';
+				// $config['source_image'] = './upload/images/' . $gbr['file_name'];
+				// $config['create_thumb'] = FALSE;
+				// $config['maintain_ratio'] = FALSE;
+				// $config['quality'] = '60%';
+				// $config['width'] = 500;
+				// $config['height'] = 400;
+				// $config['new_image'] = './upload/images/' . $gbr['file_name'];
+				// $this->load->library('image_lib', $config);
+				// $this->image_lib->resize();
 
 				$gambar = $gbr['file_name'];
 				$judul = strip_tags($this->input->post('xjudul'));
